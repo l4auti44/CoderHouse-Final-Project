133e76e514078255dc15a20791eed1a4f8b077e3
l4auti44 <lautarolamberti.44@gmail.com>
2023-04-28 15:33:24 -0300

    added basic movement and camera settings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed, turnSpeed;
+    private Animator playerAnimator;
+
+    private bool running;
+    private float count;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,10 +28,20 @@ public class PlayerController : MonoBehaviour
         var dir = new Vector3(x, 0, z).normalized;
 
         transform.Translate(dir * speed * Time.deltaTime, Space.World);
-        if (dir != Vector3.zero)
+        bool isRunning = dir != Vector3.zero;
+        switch (isRunning)
         {
-            Quaternion rot = Quaternion.LookRotation(dir, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+            case true:
+                Quaternion rot = Quaternion.LookRotation(dir, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+                playerAnimator.SetBool("Running", true);
+                break;
+            case false:
+                playerAnimator.SetBool("Running", false);
+                break;
         }
+
     }
+
+
 }
